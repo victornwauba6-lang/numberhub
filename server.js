6b0b5ec94356315eca62cb6d0266fc0e0a77c29f
NumberHub Developer <victornwauba6@gmail.com>
2026-08-16 00:40:29 +0100

    Prepare NumberHub for Render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const DATA_FILE = path.join(__dirname, "users.json");
 
 if (!fs.existsSync(DATA_FILE)) {
