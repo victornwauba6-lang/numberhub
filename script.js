b3a1ae7a6bf8fcec2e941f5a144157beb01bd8eb
victornwauba6-lang <victornwauba6@gmail.com>
2026-08-28 21:27:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 async function checkBackend() {
   try {
-    const response = await fetch("http://127.0.0.1:3001/api/status");
+    const response = await fetch("https://numberhub.onrender.com/api/status");
     const data = await response.json();
 
     console.log("NumberHub backend:", data);
